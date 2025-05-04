c28c56a24f172b472d5115d9cdcf2b857f91be28
nh246 <nazmul210130@diit.edu.bd>
2025-05-04 22:44:56 +0600

    addes navbar

---
+++ startup_post/app/(root)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+    <h2 className="text-3xl">HOME</h2>
+    </>
+  );
+}
---
+++ yc_directory/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <>
+    <h2 className="text-3xl">HOME</h2>
+    </>
+  );
+}
